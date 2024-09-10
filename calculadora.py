cc44433153979e6f65ccb32eab688994dcdd4366
Mar972310 <maria.torres-m@mail.escuelaing.edu.co>
2024-09-10 05:22:19 -0500

    retos de programación

--- calculadora.py
+++ calculadora.py
@@ -12,6 +12,11 @@ def dimension_matrices(M1,M2):
         return True
     return False
 
+def compatible_matrices(M1,M2):
+    if M1.shape[1] == M2.shape(0):
+        return True
+    return False
+
 def add_complex_vectors(v1,v2):
     if dimension_vectors(v1,v2) == True:
         return np.add(v1,v2)
@@ -52,6 +57,44 @@ def adjunct_matrix_or_vector(a):
     adjunct = np.conjugate(a.T)
     return adjunct
 
+def product_complex_matrix(M1,M2):
+    if compatible_matrices(M1,M2) == True:
+        multiplication = np.dot(M1,M2)
+        return multiplication
+    else:
+        raise Exception ("Error: Las matrices no son compatibles")
+
+def action_matix_vector(v,M):
+    return np.dot(M,v)
+
+def product_int_vectors(v1,v2):
+    product = np.vdot(v1,v2)
+    return product
+
+def norm_vec(v1):
+    return np.linalg.norm(v1)
+
+def distance(v1,v2):
+    return np.linalg.norm(v1 - v2)
+
+def values_vector(v1):
+    eigenvalues, eigenvectors = np.linalg.eig(v1)
+    return eigenvalues, eigenvectors
+
+def unit_matrix(M):
+    unit = product_complex_matrix(adjunct_matrix_or_vector(M),M)
+    if np.allclose(unit, np.eye(M.shape[0], dtype=complex)):
+        return True
+    return False
+
+def hermitian_matrix(M):
+    if np.array_equal(adjunct_matrix_or_vector(M),M):
+        return True
+    return False
+
+def tensor_matrix_or_vector(a,b):
+    return np.kron(a,b)
+       
 
        
 
